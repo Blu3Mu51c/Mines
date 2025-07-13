b0dd42d37ceabfb267a02c1ed7b9d3cb4ae0f1b3
hamza <hamzaiqbal146@gmail.com>
2025-07-14 00:32:03 +0300

    Adding Flag function upon right click

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const gridElement = document.querySelector('.grid');
 let gridSize = 9;
 let numberOfMines = 9;
 
+const tileStatus = {
+    hidden:'hidden',
+    mine:'mine',
+    number:'number',
+    marked:'marked',
+}
 
 //Creating Grid
 function createGrid(gridSize, numberOfMines){
@@ -12,12 +18,18 @@ function createGrid(gridSize, numberOfMines){
     const row =[];
     for(let cols=0; cols<gridSize; cols++){
       const individualTile = document.createElement('div');
-
+      individualTile.dataset.status = tileStatus.hidden;
       
       const tiles = {
         individualTile,
         cols,
         rows,
+        get status(){
+          return individualTile.dataset.status;
+        },
+        set status(value){
+          individualTile.dataset.status = value;
+        }
       }
 
       row.push(tiles)
@@ -27,6 +39,28 @@ function createGrid(gridSize, numberOfMines){
   return grid;
 }
 
+function flagged(tiles){
+  if (tiles.status !== tileStatus.hidden && tiles.status !==tileStatus.marked){
+    return
+  }
+  if (tiles.status === tileStatus.marked){
+    tiles.status = tileStatus.hidden;
+    tiles.individualTile.textContent = '';
+  }
+  else {
+    tiles.status = tileStatus.marked;
+    tiles.individualTile.textContent = 'O';
+  }
+}
+
+
+
+
+
+
+
+
+
 
 
 //Rendering the grid
@@ -34,6 +68,14 @@ const grid = createGrid(gridSize,numberOfMines);
 grid.forEach(row=>{
   row.forEach(tiles=>{
     gridElement.append(tiles.individualTile);
+      tiles.individualTile.addEventListener('click', ()=>{
+
+    })
+    tiles.individualTile.addEventListener('contextmenu',e =>{
+      e.preventDefault();
+      flagged(tiles);
+
+    })
   })
 })
 
